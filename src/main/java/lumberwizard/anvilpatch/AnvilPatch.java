2c30a2dcc49b66f583a602024881506965b745dd
Yotam Gur <yotamgur06@gmail.com>
2019-05-25 17:51:25 +0300

    version bump

--- src/main/java/lumberwizard/anvilpatch/AnvilPatch.java
+++ src/main/java/lumberwizard/anvilpatch/AnvilPatch.java
@@ -21,7 +21,7 @@ public class AnvilPatch
 {
     public static final String MODID = "anvilpatch";
     public static final String NAME = "Anvil Patch - Lawful";
-    public static final String VERSION = "0.2.3";
+    public static final String VERSION = "1.0.0";
 
     @SidedProxy(clientSide = "lumberwizard.anvilpatch.client.ClientProxy", serverSide = "lumberwizard.anvilpatch.common.CommonProxy")
     public static CommonProxy proxy;
